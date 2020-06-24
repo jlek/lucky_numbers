0273cccbb13335510caacae8bb774f30f45c049d
John <jlek@findmypast.com>
2020-06-24 16:36:16 +0100

    Set up lucky_numbers route

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 #![deny(clippy::all, clippy::pedantic)]
 
+use serde::Deserialize;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let lucky_numbers_route = warp::path("lucky_numbers")
+        .and(warp::query::<LuckyNumbersRequest>())
+        .map(|request: LuckyNumbersRequest| format!("{}", request.sequence));
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(lucky_numbers_route)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+
+#[derive(Deserialize)]
+struct LuckyNumbersRequest {
+    start: i32,
+    end: i32,
+    sequence: i32,
 }
