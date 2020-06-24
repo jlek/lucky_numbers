2a7fca6bd34dcaa6096443aa2daf0435adac9a4e
John <jlek@findmypast.com>
2020-06-24 16:55:48 +0100

    First stab at count_lucky_numbers implementation

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,18 @@ use warp::Filter;
 async fn main() {
     let lucky_numbers_route = warp::path("lucky_numbers")
         .and(warp::query::<LuckyNumbersRequest>())
-        .map(|request: LuckyNumbersRequest| format!("{}", request.sequence));
+        .map(|request: LuckyNumbersRequest| format!("{}", count_lucky_numbers(&request)));
 
     warp::serve(lucky_numbers_route)
         .run(([127, 0, 0, 1], 3030))
         .await;
+}
+
+fn count_lucky_numbers(request: &LuckyNumbersRequest) -> usize {
+    let sequence_string = request.sequence.to_string();
+    (request.start..=request.end).fold(0, |accumulator, number| {
+        accumulator + number.to_string().matches(&sequence_string).count()
+    })
 }
 
 #[derive(Deserialize)]
