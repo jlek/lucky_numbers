dbf9d186bdbe63258b3d2497d1687df7084fd7da
John <jlek@findmypast.com>
2020-06-25 15:44:30 +0100

    Minor tidy-up

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,21 @@ use warp::Filter;
 async fn main() {
     let lucky_numbers_route = warp::path("lucky_numbers")
         .and(warp::query::<LuckyNumbersRequest>())
-        .map(|request: LuckyNumbersRequest| format!("{}", count_lucky_numbers(&request)));
+        .map(count_lucky_numbers);
 
     warp::serve(lucky_numbers_route)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
 
-fn count_lucky_numbers(request: &LuckyNumbersRequest) -> usize {
+#[allow(clippy::needless_pass_by_value)] // Accept the request by value: the request is consumed to produce the response
+fn count_lucky_numbers(request: LuckyNumbersRequest) -> String {
     let sequence_string = request.sequence.to_string();
-    (request.start..=request.end).fold(0, |accumulator, number| {
-        accumulator + number.to_string().matches(&sequence_string).count()
-    })
+    (request.start..=request.end)
+        .fold(0, |accumulator, number| {
+            accumulator + number.to_string().matches(&sequence_string).count()
+        })
+        .to_string()
 }
 
 #[derive(Deserialize)]
