9546cdd85b4187383a052ff276e656df21906421
John <jlek@findmypast.com>
2020-06-29 17:12:46 +0100

    Tidied up code

--- src/lib.rs
+++ src/lib.rs
@@ -25,27 +25,25 @@ pub fn count_lucky_numbers(request: LuckyNumbersRequest) -> String {
 }
 
 pub fn number_of_matches(number: &[u8], sequence: &[u8]) -> usize {
-  let mut result = 0;
-
   // If the sequence does not "fit" in the number, there can be no matches
   if sequence.len() > number.len() {
     return 0;
   }
 
-  for i in 0..number.len() - sequence.len() + 1 {
-    let num_char = number[i];
-    let seq_char = sequence[0];
+  let mut result = 0;
+  let max_possible_start_index = number.len() - sequence.len() + 1;
 
-    if num_char == seq_char {
-      for j in 1..sequence.len() {
-        let num_char = number[i + j];
-        let seq_char = sequence[j];
-        if num_char != seq_char {
-          break;
-        }
-        if j == (sequence.len() - 1) {
-          result += 1;
-        }
+  for match_start_index in 0..max_possible_start_index {
+    for sequence_index in 0..sequence.len() {
+      let number_character = number[match_start_index + sequence_index];
+      let sequence_character = sequence[sequence_index];
+      if number_character != sequence_character {
+        break;
+      }
+
+      let is_last_character = sequence_index == (sequence.len() - 1);
+      if is_last_character {
+        result += 1;
       }
     }
   }
